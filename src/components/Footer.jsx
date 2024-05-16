72b6938a9ec018242e99056df4941c775d61ebbe
Benjamin Ormeño <benja.ormeno.lr@gmail.com>
2024-05-15 21:54:57 -0300

    empezando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='flex w-[85%] items-center justify-center h-screen text-white font-cuerpo dark:text-black'>
-      <p>Footer</p>
+    <footer className='flex w-[85%]  justify-evenly text-white font-cuerpo dark:text-black'>
+      <div className='flex mb-0 px-0 py-0'>
+        <h2>Copiryght</h2>
+
+      </div>
+      <nav className='flex '>
+            <ul className="flex [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-2 [&>li>a]:font-semibold   ">
+                <li><a className="hover:bg-slate-700 rounded transition-all ease-in-out duration-300" href="#Inicio">Inicio</a> </li>
+                <li><a className="hover:bg-slate-700 rounded transition-all ease-in-out duration-300" href="#SobreMi">Sobre Mi</a> </li>
+                <li><a className="hover:bg-slate-700 rounded transition-all ease-in-out duration-300" href="#Proyectos">Proyectos</a> </li>
+                <li><a className="hover:bg-slate-700 rounded transition-all ease-in-out duration-300" href="#Contacto">Contacto</a> </li>
+            </ul>
+        </nav>
     </footer>
   )
 }
--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import peliculas from '../img/peliculas.png'
 
 export default function Proyectos() {
   return (
     <section id='Proyectos' className='flex w-[85%] items-center justify-center h-screen text-white font-cuerpo dark:text-black '>
-        <p>Proyectos</p>
+        <div className='border-2 border-white px-5 py-3  flex flex-col items-center justify-center ' >
+          <img className=' w-72 z-10' src={peliculas} alt="" />
+          <h3 className='text-2xl'>Peliculas API</h3>
+          <p>Pagina con informacion de peliculas con API</p>
+          
+        </div>
     </section>
   )
 }
